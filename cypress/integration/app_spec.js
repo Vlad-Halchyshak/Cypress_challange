a6edfd101b3d0a5f9a49b2e6b122c337b3fe8c11
Vlad <vladhalchyshak@gmail.com>
2021-08-11 14:18:30 +0300

    cypress test fixing

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -15,14 +15,18 @@ describe('Sign Up', () => {
       .select('core')
       .should('have.value', 'core')
 
-    cy.get('select[name="course"]')
+    cy.get('select[name="course"]').should('exist')
       .select('git-it')
       .should('have.value', 'git-it')
 
     cy.get('input[type="submit"]')
-      .click()
+      .click().then(() => {
+        cy.contains('Saving...').should('be.visible')
+        cy.contains('Saved!', {timeout: 10000}).should('be.visible')
+      })
 
     cy.get('li')
       .should('contain', 'Some Name - some@email.com - core - git-it')
+    
   })
 })
